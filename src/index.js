63aee60ad1e98cf023d48989b087fe2e9e1837c1
MiriamOliver <miriamoliver99@gmail.com>
2022-11-14 21:22:17 +0100

    revision

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
 import './css/components.css'
 import * as crud from './js/componentes';
-//import {infoPersonajes} from './js/componentes';
 
 crud.infoPersonajes();
 crud.guardarPersonajes();
 crud.modificarPersonajes();
 crud.borrarPersonajes();
-
-/* CRUD.getUsuario(1).then(console.log);
-CRUD.crearUsuario({
-    name: 'Inma',
-    job: 'Software Engineer'
-}).then(console.log);
-
-CRUD.actualizarUsuario(1, {
-    name: 'Melisa',
-    job: 'DevOps'
-}).then(console.log);
-
-CRUD.borrarUsuario(1).then(console.log); */
--- src/js/componentes.js
+++ src/js/componentes.js
@@ -148,5 +148,3 @@ const crearHTMLresultado = (result) => {
     div.innerHTML = htmlPers;
     divAnimales.append(div.lastElementChild);
 }
-
-
